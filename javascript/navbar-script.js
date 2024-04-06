5931a1c96c0801d449080909d9ee0439635fbf25
martonhanti <212096@buas.nl>
2024-04-06 19:21:27 +0200

    gf

--- javascript/navbar-script.js
+++ javascript/navbar-script.js
@@ -20,3 +20,15 @@ document.addEventListener('DOMContentLoaded', function() {
     }, 9040); // 5000 milliseconds = 5 seconds
 });
 
+// Wait for the document to fully load
+document.addEventListener('DOMContentLoaded', function() {
+    // Set a timeout to show the navbar after 5 seconds (for example)
+    setTimeout(function() {
+        // Get the navbar element
+        var button = document.getElementById('button');
+        // Set its display property to 'block' to make it visible
+        button.style.display = 'block';
+    }, 9040); // 5000 milliseconds = 5 seconds
+});
+
+
