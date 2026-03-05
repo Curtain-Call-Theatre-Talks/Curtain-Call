a7a67b73ba0ad1175b07f3326060e2ae916ceb84
martonhanti <212096@buas.nl>
2026-03-05 14:40:26 +0100

    skip button

--- javascript/navbar-script.js
+++ javascript/navbar-script.js
@@ -1,34 +1,39 @@
-// Wait for the document to fully load
-document.addEventListener('DOMContentLoaded', function() {
-    // Set a timeout to show the navbar after 5 seconds (for example)
-    setTimeout(function() {
-        // Get the navbar element
-        var navbar = document.getElementById('navbar');
-        // Set its display property to 'block' to make it visible
-        navbar.style.display = 'block';
-    }, 9040); // 5000 milliseconds = 5 seconds
+document.addEventListener('DOMContentLoaded', function () {
+
+    var navbar = document.getElementById('navbar');
+    var footer = document.getElementById('footer');
+    var button = document.getElementById('button');
+    var skip = document.getElementById('skipIntro');
+
+function showContent() {
+
+    navbar.style.display = 'block';
+    footer.style.display = 'block';
+    button.style.display = 'block';
+
+    navbar.style.opacity = 0;
+    footer.style.opacity = 0;
+
+    setTimeout(function(){
+        navbar.style.transition = "opacity 0.8s";
+        footer.style.transition = "opacity 0.8s";
+
+        navbar.style.opacity = 1;
+        footer.style.opacity = 1;
+    },10);
+
+    if(skip){
+        // fade out instead of instantly disappearing
+        skip.classList.add("hidden");
+    }
+}
+
+    // automatic intro finish
+    setTimeout(showContent, 9040);
+
+    // skip intro click
+    if(skip){
+        skip.addEventListener("click", showContent);
+    }
+
 });
-
-// Wait for the document to fully load
-document.addEventListener('DOMContentLoaded', function() {
-    // Set a timeout to show the navbar after 5 seconds (for example)
-    setTimeout(function() {
-        // Get the navbar element
-        var footer = document.getElementById('footer');
-        // Set its display property to 'block' to make it visible
-        footer.style.display = 'block';
-    }, 9040); // 5000 milliseconds = 5 seconds
-});
-
-// Wait for the document to fully load
-document.addEventListener('DOMContentLoaded', function() {
-    // Set a timeout to show the navbar after 5 seconds (for example)
-    setTimeout(function() {
-        // Get the navbar element
-        var button = document.getElementById('button');
-        // Set its display property to 'block' to make it visible
-        button.style.display = 'block';
-    }, 9040); // 5000 milliseconds = 5 seconds
-});
-
-
